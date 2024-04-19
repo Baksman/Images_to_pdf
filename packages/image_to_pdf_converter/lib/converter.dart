309187ec4b8e783977534aa40d44fc37c9365f91
Ibrahim shehu <ibrahimshehu73700@gmail.com>
2024-04-19 16:13:02 +0100

    Update converter.dart

--- packages/image_to_pdf_converter/lib/converter.dart
+++ packages/image_to_pdf_converter/lib/converter.dart
@@ -15,7 +15,7 @@ class ImageToPdf {
         pdf.addPage(
           pw.Page(
             build: (pw.Context context) {
-              return pw.Image(pdfImage,fit:pw.BoxFit.cover); // Center
+              return pw.Image(pdfImage,fit:pw.BoxFit.fitWidth); // Center
             },
           ),
         );
